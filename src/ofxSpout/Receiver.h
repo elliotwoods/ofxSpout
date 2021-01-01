64f8dbc3eb8c326f7c6a1b1ced974fa63b2e2b67
medcelerate <32549017+medcelerate@users.noreply.github.com>
2021-01-01 15:07:04 -0500

    Update Receiver.h
    Fixed pointers and types

--- src/ofxSpout/Receiver.h
+++ src/ofxSpout/Receiver.h
@@ -16,19 +16,19 @@ namespace ofxSpout {
 		void release();
 		bool isInitialized() const;
 
-		bool receive(ofTexture &); // will automatically allocate the texture
+		bool receive(ofTexture&); // will automatically allocate the texture
 
 		bool selectSenderPanel();
 
 		std::string getChannelName() const;
 		float getWidth() const;
 		float getHeight() const;
-		
-		vector<std::string> getAvailableSenders;
-		
+
+		std::vector<std::string> getAvailableSenders;
+
 
 	protected:
-		SpoutReceiver * spoutReceiver;
+		SpoutReceiver* spoutReceiver;
 
 		const int defaultFormat; // = GL_RGBA. If you're using anything else then make sure to allocate your texture manually
 	};
